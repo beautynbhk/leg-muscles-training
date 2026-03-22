6525237f8440e3851d4a382160dcceae712bbd9f
beautynbhk <beautynb.hk@gmail.com>
2026-03-22 18:02:20 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+export const metadata = {
+  title: '右下肢矯正 App',
+  description: '右側下肢矯正訓練計畫',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh-TW">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
